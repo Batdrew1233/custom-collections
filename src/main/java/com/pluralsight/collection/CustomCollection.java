de7eca7674c5d4efdbf14db2ba2f936ed21bb915
Batdrew1233 <drewstevenson1233@gmail.com>
2026-05-18 12:56:24 -0400

    Testing

--- src/main/java/com/pluralsight/collection/CustomCollection.java
+++ src/main/java/com/pluralsight/collection/CustomCollection.java
@@ -1,5 +1,7 @@
 package com.pluralsight.collection;
 
+import java.time.LocalDate;
+
 public class CustomCollection {
     public static void main(String[] args) {
         FixedList<Integer> numbers = new FixedList<>(3);
@@ -8,5 +10,11 @@ public class CustomCollection {
         numbers.add(92);
         numbers.add(43);
         System.out.println(numbers.getItems());
+
+        FixedList<LocalDate> dates = new FixedList<>(2);
+        dates.add(LocalDate.now());
+        dates.add(LocalDate.now());
+        dates.add(LocalDate.now().withDayOfMonth(1));
+        System.out.println(dates.getItems());
     }
 }
